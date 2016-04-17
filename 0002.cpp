c3214c75d03ebffc5f6166cf29aa817b80088bee
tkmru <i.am.tkmru@gmail.com>
2016-04-18 02:56:46 +0900

    0000-0007.cpp

---
+++ 0002.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    while (cin >> a >> b) {
+        int digit = 1;
+        int sum = a + b;
+        while (true) {
+            if (sum / 10 != 0) {
+                digit += 1;
+                sum /= 10;
+            }
+ 
+            else if (sum / 10 == 0 ) {
+                break;
+            }
+        }
+        cout << digit << endl;
+    }
+
+    return 0;
+}
---
+++ 0006.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+ 
+int main(){
+    string a;
+    cin >> a;
+    for(int i = a.size(); i >= 0; i--){
+        cout << a[i];
+    }
+    cout << endl;
+
+    return 0;
+}
